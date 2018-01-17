78a0f40ffc627063e012ffb3d020a82ea40506d2
Pavel Korolev <dev@borodust.org>
2018-01-17 09:13:26 +0300

    Update :claw library descriptor

--- claw.lisp
+++ claw.lisp
@@ -1,11 +1,12 @@
 (claw:c-include "bodge_async.h" :bodge-async
-  :package :%uv
+  :in-package :%uv
   :include-sources ("uv\\.h")
   :include-definitions ("^uv_(?!_|buf_init).*$"
                         "^ssize_t$")
-  :symbol-exceptions (("getaddrinfo" platform-getaddrinfo)
-                      ("freeaddrinfo" platform-freeaddrinfo)
-                      ("gid_t" platform-gid-t)
-                      ("uid_t" platform-uid-t)
-                      ("sem_t" platform-sem-t))
-  :symbol-prefix ("uv_" "UV_"))
+  :rename-symbols (claw:in-pipeline
+                   (claw:by-changing "getaddrinfo" 'platform-getaddrinfo)
+                   (claw:by-changing "freeaddrinfo" 'platform-freeaddrinfo)
+                   (claw:by-changing "gid_t" 'platform-gid-t)
+                   (claw:by-changing "uid_t" 'platform-uid-t)
+                   (claw:by-changing "sem_t" 'platform-sem-t)
+                   (claw:by-removing-prefixes "uv_" "UV_")))
